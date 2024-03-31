714c96290379c5bc1d0efe8006427678f24eaa4e
illidanCStormrage <2636538012@qq.com>
2024-03-31 09:03:27 +0800

    StormrageToolkit initiated

---
+++ app/src/main/java/com/illidancstormrage/mainapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.illidancstormrage.mainapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.illidancstormrage.mainapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
